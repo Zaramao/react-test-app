de5c1cdc88191aa8d8ac5b665cb14b443355f050
Ivan Drenjanin <k33borg@gmail.com>
2017-10-27 00:36:07 +0200

    webpack.config issue

--- webpack.config.js
+++ webpack.config.js
@@ -3,9 +3,9 @@ const webpack = 'webpack';
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 process.env.NODE_ENV = process.env.NODE_ENV || 'devemopment';
 
-if (process.env.NODE_ENV === test) {
+if (process.env.NODE_ENV === 'test') {
 	require('dotenv').config({ path: '.env.test' });
-} else if (process.env.NODE_ENV === development) {
+} else if (process.env.NODE_ENV === 'development') {
 	require('dotenv').config({ path: '.env.development' });
 }
 
