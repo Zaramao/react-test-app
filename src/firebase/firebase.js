27e5c225dde8aaf523749f46d31ee8f633f55492
Ivan Drenjanin <k33borg@gmail.com>
2017-10-27 00:32:19 +0200

    setup test database env

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,22 +1,15 @@
 import * as firebase from 'firebase';
 
 const config = {
-	apiKey: 'AIzaSyA_GT_rCK6DG3J9vztlRVxo5KRxWva5Q-c',
-	authDomain: 'expensify-3a26e.firebaseapp.com',
-	databaseURL: 'https://expensify-3a26e.firebaseio.com',
-	projectId: 'expensify-3a26e',
-	storageBucket: 'expensify-3a26e.appspot.com',
-	messagingSenderId: '118582479458'
+	apiKey: process.env.FIREBASE_API_KEY,
+	authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+	databaseURL: process.env.FIREBASE_DATABASE_URL,
+	projectId: process.env.FIREBASE_PROJECT_ID,
+	storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+	messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
 };
 
 firebase.initializeApp(config);
 const database = firebase.database();
 
-database.ref().set({
-	name: 'Ivan',
-	isGood: true,
-	location: {
-		city: 'Belgrade',
-		country: 'Serbia'
-	}
-});
+export { firebase, database as default };
