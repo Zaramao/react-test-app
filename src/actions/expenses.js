eef738923fd0f41f416e633966b70fe64e46328f
Ivan Drenjanin <k33borg@gmail.com>
2017-10-27 13:22:36 +0200

    get firebase expenses on start

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -10,12 +10,8 @@ export const addExpense = (expense) => ({
 export const startAddExpense = (expenseData = {}) => {
 	return (dispatch) => {
 		const { description = '', note = '', amount = 0, createdAt = 0 } = expenseData;
-		const expense = {
-			description,
-			note,
-			amount,
-			createdAt
-		};
+		const expense = { description, note, amount, createdAt };
+
 		return database.ref('expenses').push(expense).then((ref) => {
 			dispatch(
 				addExpense({
@@ -39,3 +35,26 @@ export const editExpense = (id, updates) => ({
 	id,
 	updates
 });
+
+// SET_EXPENSES
+export const setExpenses = (expenses) => ({
+	type: 'SET_EXPENSES',
+	expenses
+});
+
+export const startSetExpenses = () => {
+	return (dispatch) => {
+		return database.ref('expenses').once('value').then((snapshot) => {
+			const expenses = [];
+
+			snapshot.forEach((childSnapshot) => {
+				expenses.push({
+					id: childSnapshot.key,
+					...childSnapshot.val()
+				});
+			});
+
+			dispatch(setExpenses(expenses));
+		});
+	};
+};
